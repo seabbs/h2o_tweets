bc8554f09336a5b154fd3dca351a90776a50fb9d
Sam Abbott <s.e.abbott12@gmail.com>
2018-03-11 19:04:43 +0000

    basic code

---
+++ R/set_up_script.R
@@ -0,0 +1,13 @@
+## Get required packages
+install.packages("devtools")
+install.packages("cronR")
+devtools::install_github("seabbs/StackTweetBot")
+
+
+## Add twitter API access
+
+
+## Add stack overflow api access
+
+
+## Set up, save and schedule h2o bot
