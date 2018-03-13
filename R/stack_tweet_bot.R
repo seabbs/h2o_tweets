baf18013ee8def8f074f8e6bee0d528e904a8e7b
Sam Abbott <s.e.abbott12@gmail.com>
2018-03-13 14:01:58 +0000

    bot updates

--- R/stack_tweet_bot.R
+++ R/stack_tweet_bot.R
@@ -1,8 +1,15 @@
 library(StackTweetBot);
+
+message('Getting Stack Overflow questions at ', Sys.time());
+
 questions <- get_stack_questions(extracted_tags = 'h2o',
                                  excluded_tags = NULL,
                                  time_window = 15,
                                  add_process_fn = NULL);
+
+message('Extracted ', length(questions$title), ' questions');
+
+message('Posting questions as tweets');
 
 posts <- post_stack_tweets(questions, hashtags = c('datascience', 'machinelearning', 'stackoverflow'),
                            post = TRUE);
